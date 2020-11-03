ec30fbab31f1114bc44e661f305c373db800eece
unknown <khushalthepane2000@gmail.com>
2020-11-04 00:22:14 +0530

    product page

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen';
+import ProdiuctScreen from './screens/ProdiuctScreen';
 
 function App() {
   return (
-    <div className="">
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
-        </Container>
-        
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProdiuctScreen} />
+        </Container>   
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- e-commerce/src/components/Header.js
+++ e-commerce/src/components/Header.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header>
             <Navbar bg="success" variant="light" expand="lg" collapseOnSelect>
                 <Container>
-                <Navbar.Brand href="/">Pro-Shop</Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Pro-Shop</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                    <Nav.Link style={{color: 'black'}} href="/"><i className="fas fa-shopping-cart pr-1"></i> Cart</Nav.Link>
-                    <Nav.Link style={{color: 'black'}} href="/"><i className="fas fa-user pr-1"></i> Sign In</Nav.Link>
+                    <LinkContainer style={{color: 'black'}} to="/cart">
+                        <Nav.Link>
+                            <i className="fas fa-shopping-cart pr-1"></i> Cart
+                        </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer style={{color: 'black'}} to="/login">
+                        <Nav.Link>
+                            <i className="fas fa-user pr-1"></i> Sign In
+                        </Nav.Link>
+                    </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
