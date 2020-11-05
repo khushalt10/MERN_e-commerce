8ce440896cc0672d0c879348e3e388a6d2a921b5
unknown <khushalthepane2000@gmail.com>
2020-11-05 16:40:20 +0530

    MOngo Connected

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import products from'./data/products.js'
+import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app = express()
+
+connectDB()
 
 app.get('/', (req,res) => {
     res.send("Hello Wolrd")
