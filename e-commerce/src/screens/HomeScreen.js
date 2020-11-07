e727e952eb455fd7922058eff6259196424e1a05
unknown <khushalthepane2000@gmail.com>
2020-11-07 19:44:35 +0530

    loader and Message

--- e-commerce/src/screens/HomeScreen.js
+++ e-commerce/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { Col, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function HomeScreen() {
     const dispatch = useDispatch()
@@ -17,7 +19,7 @@ function HomeScreen() {
     return (
         <>
             <h1>Latest Products</h1>
-            {loading ? <h2>Loading....</h2> : error ? <h3>{error}</h3> : (
+            {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
             <Row>
                 {products.map(product => (
                     <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
