f4d7b0ea3d93297eabe7d23d65ed4712447ec63f
unknown <khushalthepane2000@gmail.com>
2020-11-16 00:02:18 +0530

    ypdateorderTo Paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -36,8 +36,32 @@ const  getOrderById = asyncHandler(async(req, res) => {
     }
 })
 
+const updateOrderToPaid = asyncHandler(async(req, res) => {
+    const order = await Order.findById(req.params.id)
+
+    if (order) {
+        order.isPaid = true
+        order.paidAt = Date.now()
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updaredOrder = await order.save()
+
+        res.json(updaredOrder)
+    } else {
+        res.status(404)
+        throw new Error('Order not Found')
+    }
+})
+
+
 
 export {
    addOrderItems,
-   getOrderById
+   getOrderById,
+   updateOrderToPaid
 }
