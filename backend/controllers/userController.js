401cf5b5927c8a13d64f5ababfabb8f063b18249
unknown <khushalthepane2000@gmail.com>
2020-11-19 00:03:47 +0530

    getuserbyid and update user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,6 +112,38 @@ const deleteUser = asyncHandler(async(req, res) => {
     
 })
 
+const getUserById = asyncHandler(async(req, res) => { 
+    const user = await User.findById(req.params.id).select('-password')
+    if (user) {
+        res.json(user)
+    } else {
+        res.status(404)
+        throw new Error('User not Found')
+    }
+})
+
+const updateUser = asyncHandler(async(req, res) => { 
+    const user = await User.findById(req.params.id)
+    
+    if(user) {
+        user.name = req.body.name || user.name
+        user.email = req.body.email || user.email
+        user.isAdmin = req.body.isAdmin || user.isAdmin
+
+        const updatedUser = await user.save()
+
+        res.json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin
+        })
+        
+    }else {
+        res.status(404)
+        throw new Error('User not Found')
+    }
+})
 
 export {
     authUser,
@@ -119,5 +151,7 @@ export {
     registerUser,
     updateUserProfile,
     getAllUsers,
-    deleteUser
+    deleteUser,
+    getUserById,
+    updateUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import Product from '../models/productModel.js'
 import asyncHandler from 'express-async-handler'
-import { authUser, getUserProfile, registerUser, updateUserProfile, getAllUsers, deleteUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getAllUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
@@ -15,5 +15,6 @@ router.post('/login', authUser)
 //Authorize
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
-router.route('/:id').delete(protect, admin, deleteUser)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
+
 export default router
